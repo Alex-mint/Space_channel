2e2fa1a45176ca0fb525af435bcd5f33ac559f6a
Gato Negro <62758814+Alex-mint@users.noreply.github.com>
2021-08-20 14:39:23 +0200

    Исправил ошибки, высушил код
    - Исправил составные существительные
    - Повысил надежность операций с файлами
    - Поправил множественное число в названиях
    - Починил извлечение filename из url
    - Уточнил название функции
    - Высушил код

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import os
 import requests
 import time
 import telegram
-
 from os import listdir
 from urllib import parse
 from dotenv import load_dotenv
 
 
-def fetch_spacex_last_launch(path):
+def fetch_spacex_last_launch_links(path):
     spacex_url = "https://api.spacexdata.com/v4/launches/latest"
     response = requests.get(spacex_url)
     response.raise_for_status()
     links = response.json()["links"]["flickr"]["original"]
-    image_name = "spacex"
-    for link_number, link in enumerate(links):
-        download_image(path, link, link_number, image_name)
+    download_image(path, links)
 
 
 def get_extension(url):
-    extension = os.path.splitext(url)[-1]
+    unquoted_url = parse.unquote(url)
+    path = parse.urlparse(unquoted_url).path
+    extension = os.path.splitext(path)[-1]
     return extension
 
 
@@ -36,34 +35,39 @@ def get_image_links(nasa_url, nasa_api_key):
     return images_links
 
 
-def download_image(path, link, link_number, image_name, payload=None):
-    response = requests.get(link, params=payload)
-    response.raise_for_status()
-    extension = get_extension(link)
-    with open(f"{path}{image_name}{link_number}{extension}", "wb") as file:
-        file.write(response.content)
+def download_image(path, links, payload=None):
+    image_name = path.split("_")[0]
+    path = os.path.join(path, image_name)
+    for link_number, link in enumerate(links):
+        extension = get_extension(link)
+        response = requests.get(link, params=payload)
+        response.raise_for_status()
+        with open(f"{path}{link_number}{extension}", "wb") as file:
+            file.write(response.content)
 
 
-def fetch_nasa_image(path, nasa_api_key):
+def fetch_nasa_images_links(path, nasa_api_key):
     nasa_url = "https://api.nasa.gov/planetary/apod"
-    image_name = "nasa"
-    for link_number, link in enumerate(get_image_links(nasa_url, nasa_api_key)):
-        if parse.urlparse(link).netloc == "apod.nasa.gov":
-            download_image(path, link, link_number, image_name)
+    nasa_image_links = get_image_links(nasa_url, nasa_api_key)
+    for link in nasa_image_links:
+        if parse.urlparse(link).netloc != "apod.nasa.gov":
+            nasa_image_links.remove(link)
+    download_image(path, nasa_image_links)
 
 
-def fetch_epic_image(path, nasa_api_key):
+def fetch_epic_images_links(path, nasa_api_key):
     payload = {"api_key": nasa_api_key}
     nasa_epic_url = "https://api.nasa.gov/EPIC/api/natural/images"
     response = requests.get(nasa_epic_url, params=payload)
     response.raise_for_status()
     response = response.json()
+    links = []
     for image_number, image in enumerate(response):
         image_date = image["date"]
         image_name = image["image"]
         link = make_epic_image_link(image_date, image_name)
-        image_name = "epic_nasa"
-        download_image(path, link, image_number, image_name, payload=payload)
+        links.append(link)
+    download_image(path, links, payload=payload)
 
 
 def make_epic_image_link(image_date, image_name,):
@@ -73,47 +77,46 @@ def make_epic_image_link(image_date, image_name,):
     return link
 
 
-def get_image_paths(images_directories):
-    all_images_paths = []
-    for images_dir in images_directories:
+def get_image_paths(image_directories):
+    all_image_paths = []
+    for images_dir in image_directories:
         images = listdir(images_dir)
-        images_paths = [f"{images_dir}/{image}" for image in images]
-        all_images_paths += images_paths
-    return all_images_paths
+        image_paths = [f"{images_dir}/{image}" for image in images]
+        all_image_paths += image_paths
+    return all_image_paths
 
 
-def send_pictures_to_telegram(images_paths, tg_token, tg_chat_id, time_sleep):
+def send_pictures_to_telegram(image_paths, tg_token, tg_chat_id, sleep_time):
     bot = telegram.Bot(token=tg_token)
     while True:
-        for path in images_paths:
-            bot.send_document(chat_id=tg_chat_id,
-                              document=open(path, "rb"))
-            time.sleep(time_sleep)
+        for path in image_paths:
+            with open(path, "rb") as file:
+                bot.send_document(chat_id=tg_chat_id, document=open(path, "rb"))
+                time.sleep(sleep_time)
 
 
 def main():
-    time_sleep = 86400
-
     load_dotenv()
     tg_token = os.getenv("TG_TOKEN")
     tg_chat_id = os.getenv("TG_CHAT_ID")
     nasa_api_key = os.getenv("NASA_API_KEY")
 
-    images_directories = ["spacex_images", "nasa_images", "nasa_epic_images"]
-    spacex_path = "spacex_images/"
-    nasa_path = "nasa_images/"
-    nasa_epic_path = "nasa_epic_images/"
-    
-    os.makedirs("spacex_images", exist_ok=True)
-    os.makedirs("nasa_images", exist_ok=True)
-    os.makedirs("nasa_epic_images", exist_ok=True)
+    sleep_time = 86400
+    image_directories = ["spacex_images", "nasa_images", "epic_images"]
 
-    fetch_spacex_last_launch(spacex_path)
-    fetch_nasa_image(nasa_path, nasa_api_key)
-    fetch_epic_image(nasa_epic_path, nasa_api_key)
+    for directory in image_directories:
+        os.makedirs(directory, exist_ok=True)
 
-    images_paths = get_image_paths(images_directories)
-    send_pictures_to_telegram(images_paths, tg_token, tg_chat_id, time_sleep)
+    spacex_path = "spacex_images"
+    nasa_path = "nasa_images"
+    nasa_epic_path = "epic_images"
+
+    fetch_spacex_last_launch_links(spacex_path)
+    fetch_nasa_images_links(nasa_path, nasa_api_key)
+    fetch_epic_images_links(nasa_epic_path, nasa_api_key)
+
+    image_paths = get_image_paths(image_directories)
+    send_pictures_to_telegram(image_paths, tg_token, tg_chat_id, sleep_time)
 
 
 if __name__ == "__main__":
